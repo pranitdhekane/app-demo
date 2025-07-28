474aaa4be63a8103a211eac5015fed857993db3d
pranitdhekane <49141184+pranitdhekane@users.noreply.github.com>
2025-07-28 21:42:11 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull dpranit/simple-python-flask-app
+docker pull dpranit/simple-python-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 dpranit/simple-python-flask-app
+docker run -d -p 5000:5000 dpranit/simple-python-app
